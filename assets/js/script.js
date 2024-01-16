fc3fe9cfd06a283d0df7ec3018819cf22d25874c
Kid Lei <eric@collectiveequity.com>
2024-01-16 23:09:47 +0000

    tested Flight API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 var apiKey = "65a6dfbcf70ea898ea3cbc71";
 
-var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/Economy/USD`;
 
-var codeURL = `https://api.flightapi.io/iata/api-key?name=american&type=airline`
+// Logic when the search is submitted
+$("#searchFrom").on("submit",function(event){
+    event.preventDefault();
+
+})
+
+
+
+// var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${class}/${currency}`;
+var airportName = "London+Heathrow+Airport";
+var type = "airport";
+
+var codeURL = `https://api.flightapi.io/iata/api-key?${apiKey}&name=${airportName}&type=${type}`;
+
+fetch(codeURL)
+.then(function(response) {
+    return response.json();
+}).then(function(data) {
+    console.log(data);
+});
