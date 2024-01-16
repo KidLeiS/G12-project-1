1744be6e1b9908596302dcde75e16165e75dc476
Kid Lei <eric@collectiveequity.com>
2024-01-16 21:25:18 +0000

    added Flight API server GET URLs

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var apiKey = "65a6dfbcf70ea898ea3cbc71";
+
+var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/Economy/USD`;
+
+var codeURL = `https://api.flightapi.io/iata/api-key?name=american&type=airline`
