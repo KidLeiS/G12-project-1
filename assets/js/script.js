197639933899e4f263d1731fd4b0631e823015ba
Kid Lei <eric@collectiveequity.com>
2024-01-17 21:44:38 +0000

    added a check to see if the user is searching for a one way or return flight

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,31 @@
 var apiKey = "65a6dfbcf70ea898ea3cbc71";
-
+var flightType = $("#flexSwitchCheckDefault").attr("checked");
+console.log(flightType);
 
 // Logic when the search is submitted
 $("#searchFrom").on("submit",function(event){
     event.preventDefault();
 
+    var flightType = $("#flexSwitchCheckDefault").attr("checked");
+    console.log(flightType);
+
+    var dep = $("#fromSearch");
+    var arr = $("#toSearch");
+    var depDate = $("#departSearch");
+    var retDate = $("#returnSearch");
+    var adults = $("#adultSearch");
+    var childs = $("#childSearch");
+    var infants = $("#infantSearch");
+    var flightClass = $("#classSearch");
+    var currency = $("#currencySearch");
+
+
+
 })
 
 
 
-var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${class}/${currency}`;
+
 var airportName = "London+Heathrow+Airport";
 var type = "airport";
 
@@ -24,6 +40,8 @@ fetch(`https://api.flightapi.io/iata/${apiKey}?name=${airportName}&${type}`)
 
 // function for 1 way URL call
 var triggerOneway = function() {
+    var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${flightClass}/${currency}`;
+
     fetch(oneWayURL)
     .then(function(response) {
     console.log(response);
