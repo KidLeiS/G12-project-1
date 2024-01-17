d6af7dccb8d5b7bd4f69acc82c454bf665bfd676
Kid Lei <eric@collectiveequity.com>
2024-01-17 18:48:42 +0000

    started function for one way API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $("#searchFrom").on("submit",function(event){
 
 
 
-// var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${class}/${currency}`;
+var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${class}/${currency}`;
 var airportName = "London+Heathrow+Airport";
 var type = "airport";
 
@@ -21,3 +21,13 @@ fetch(`https://api.flightapi.io/iata/${apiKey}?name=${airportName}&${type}`)
 }).then(function(data){
     console.log(data);
 });
+
+// function for 1 way URL call
+var triggerOneway = function() {
+    fetch(oneWayURL)
+    .then(function(response) {
+    console.log(response);
+    }).then(function(data){
+        var searchResults = data.itineraries;
+    });
+}
