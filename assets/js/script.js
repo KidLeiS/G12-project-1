e0a90277647c721a16f7b2d5eea3495b96fafdd4
Kid Lei <eric@collectiveequity.com>
2024-01-18 19:15:49 +0000

    wrote a javascript function to pass the search variables and return a search URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var apiKey = "65a6dfbcf70ea898ea3cbc71";
 var ftypeSelector = $("#flexSwitchCheckDefault");
+var rawSearchResults;
 
 // Blacks out return date when one way is selected
 $("#returnSearch").attr("disabled",true);
@@ -23,27 +24,43 @@ ftypeSelector.on("click",function(event){
 // Logic when the search is submitted
 $("#searchButton").on("click",function(event){
     event.preventDefault();
+    var dep = $("#fromSearch").val();
+    var arr = $("#toSearch").val();
+    var depDate = $("#departSearch").val();
+    var retDate = $("#returnSearch").val();
+    var adults = $("#adults").val();
+    var childs = $("#childs").val();
+    var infants = $("#infants").val();
+    var flightClass = $("#flightClass").val();
+    var currency = $("#currency").val();
 
-    //Checks if the type selected is one way (false) or return (true)
+    //Checks if the type selected is one way (false) or return (true), and generates the raw data
     if(ftypeSelector[0].checked){
+        //Return flight
+        var twoWayURL = `https://api.flightapi.io/roundtrip/${apiKey}/${dep}/${arr}/${depDate}/${retDate}/${adults}/${childs}/${infants}/${flightClass}/${currency}`;
+        console.log(twoWayURL);
+        // Saving API calls
+        // fetch(twoWayURL)
+        // .then(function(response) {
+        // console.log(response);
+        // }).then(function(data){
+        //     rawSearchResults = data;
+        // });
+
 
     } else {
-        
+        //One way flight
+        var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${flightClass}/${currency}`;
+        console.log(oneWayURL);
+        // Saving API calls
+        // fetch(oneWayURL)
+        // .then(function(response) {
+        // console.log(response);
+        // }).then(function(data){
+        //     rawSearchResults = data;
+        // });
 
     }
-
-    var flightType = $("#flexSwitchCheckDefault");
-    console.log(flightType);
-
-    var dep = $("#fromSearch");
-    var arr = $("#toSearch");
-    var depDate = $("#departSearch");
-    var retDate = $("#returnSearch");
-    var adults = $("#adultSearch");
-    var childs = $("#childSearch");
-    var infants = $("#infantSearch");
-    var flightClass = $("#classSearch");
-    var currency = $("#currencySearch");
 
 
 
@@ -52,26 +69,14 @@ $("#searchButton").on("click",function(event){
 
 
 
-var airportName = "London+Heathrow+Airport";
-var type = "airport";
+// var airportName = "London+Heathrow+Airport";
+// var type = "airport";
 
-var codeURL = `https://api.flightapi.io/iata/${apiKey}/${airportName}/${type}`;
+// var codeURL = `https://api.flightapi.io/iata/${apiKey}/${airportName}/${type}`;
 
-fetch(`https://api.flightapi.io/iata/${apiKey}?name=${airportName}&${type}`)
-.then(function(response) {
-    console.log(response);
-}).then(function(data){
-    console.log(data);
-});
-
-// function for 1 way URL call
-var triggerOneway = function() {
-    var oneWayURL = `https://api.flightapi.io/onewaytrip/${apiKey}/${dep}/${arr}/${depDate}/${adults}/${childs}/${infants}/${flightClass}/${currency}`;
-
-    fetch(oneWayURL)
-    .then(function(response) {
-    console.log(response);
-    }).then(function(data){
-        var searchResults = data.itineraries;
-    });
-}
+// fetch(`https://api.flightapi.io/iata/${apiKey}?name=${airportName}&${type}`)
+// .then(function(response) {
+//     console.log(response);
+// }).then(function(data){
+//     console.log(data);
+// });
